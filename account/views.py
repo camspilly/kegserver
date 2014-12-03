967aa15599e01613d7b2b6c46b85dd6a4148e78d
Nathan McCloskey <nathanmccloskey@gmail.com>
2014-12-03 15:53:08 -0500

    account prototypes

--- account/views.py
+++ account/views.py
@@ -6,27 +6,38 @@ from django.template import RequestContext
 from account.models import KegUserForm, UserForm
 
 def payment(request):
-    pass
-    
-    
-def register(request):
-    if request.method == 'POST':
-        userf = UserForm(request.POST, prefix='user')
-        keguserf = KegUserForm(request.POST, prefix='keguser')
-        if userf.is_valid() * keguserf.is_valid():
-            user = userf.save(commit=False)
-            user.username = user.email
-            user.save()
-            keguser = keguserf.save(commit=False)
-            keguser.user = user
-            keguser.save()
-            return redirect('/helloworld/')
-    else:
-        userf = UserForm(prefix='user')
-        keguserf = KegUserForm(prefix='keguser')
-        return render_to_response('templates/register/register2.html', 
-                                                   dict(userform=userf,
-                                                        keguserform=keguserf),
-                                                   context_instance=RequestContext(request))
+  pass
+  
+def index(request):
+  # /account/
+  # display reset pin
+  # display add payment option
+  # manage stripe
+  # view stripe transactions?
+  pass
 
-    return redirect('/helloworld/')
+def resetpin(request):
+  pass
+
+def register(request):
+  if request.method == 'POST':
+    userf = UserForm(request.POST, prefix='user')
+    keguserf = KegUserForm(request.POST, prefix='keguser')
+    if userf.is_valid() * keguserf.is_valid():
+      user = userf.save(commit=False)
+      user.username = user.email
+      user.save()
+      keguser = keguserf.save(commit=False)
+      keguser.user = user
+      keguser.save()
+      return redirect('/helloworld/')
+  else:
+    userf = UserForm(prefix='user')
+    keguserf = KegUserForm(prefix='keguser')
+    return render_to_response('templates/register/register2.html', 
+                           dict(userform=userf,
+                            keguserform=keguserf),
+                           context_instance=RequestContext(request))
+
+  return redirect('/account/') # redirect to account, display toast that says succesful register
+
