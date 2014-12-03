ae4732068d33e504cc7c9f5e0159cc0d814832f1
Nathan McCloskey <nathanmccloskey@gmail.com>
2014-12-03 18:55:29 -0500

    title image and fixed js errors

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -1,9 +1,9 @@
-$(document).ready( {
+$(document).ready(function() {
 	$("#registerButton").on("click", function() {
 		$("#registerForm").submit();
 	});
 
 	$("#loginButton").on("click", function() {
-		$("loginForm").submit()
+		$("#loginForm").submit();
 	});
 });
